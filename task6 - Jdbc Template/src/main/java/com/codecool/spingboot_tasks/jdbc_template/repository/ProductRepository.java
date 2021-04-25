3e00d623b791a38b91dcccf9f4f70a4884df5664
Peter <peter.zmilczak@gmail.com>
2021-04-25 10:44:45 +0200

    task 6 implementions

--- task6 - Jdbc Template/src/main/java/com/codecool/spingboot_tasks/jdbc_template/repository/ProductRepository.java
+++ task6 - Jdbc Template/src/main/java/com/codecool/spingboot_tasks/jdbc_template/repository/ProductRepository.java
@@ -3,10 +3,14 @@ package com.codecool.spingboot_tasks.jdbc_template.repository;
 import com.codecool.spingboot_tasks.jdbc_template.model.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Repository
 public class ProductRepository {
@@ -25,8 +29,27 @@ public class ProductRepository {
         return template.query(query, new ProductMapper());
     }
 
+    public List<Integer> findAllPrices(){
+        String query = "SELECT price FROM products";
+        return template.queryForList(query, Integer.class);
+    }
+
+    public List<String> findAllNames(){
+        String query = "SELECT name FROM products";
+        return template.queryForList(query, String.class);
+    }
+
     public Product findById(long id) {
-        return null;
+        String query = "SELECT * FROM products WHERE id = :id";
+
+//        Alternative
+//        Map<String, Object> params = new HashMap<>();
+//        params.put("id", id);
+
+        SqlParameterSource params = new MapSqlParameterSource()
+                .addValue("id", id);
+
+        return namedTemplate.queryForObject(query, params, new ProductMapper());
     }
 
     public Product create(Product product) {
