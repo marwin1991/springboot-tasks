de68b9aa96e70895a8575b08d7156429b08e5067
Peter <peter.zmilczak@gmail.com>
2021-08-21 09:05:25 +0200

    Some small improvements

--- task7 - Swagger/src/main/java/com/codecool/spingboot_tasks/swagger/controller/ProductController.java
+++ task7 - Swagger/src/main/java/com/codecool/spingboot_tasks/swagger/controller/ProductController.java
@@ -1,9 +1,6 @@
 package com.codecool.spingboot_tasks.swagger.controller;
 
 import com.codecool.spingboot_tasks.swagger.model.Product;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
@@ -12,10 +9,7 @@ import java.util.List;
 @RestController
 public class ProductController {
 
-    @ApiOperation("Operation to list all products")
-    @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Powodzenie / Success", response = Product.class),
-            @ApiResponse(code = 400, message = "Błędne żądanie / Bad request", response = Error.class)})
+
     @GetMapping("/products")
     public List<Product> getProducts() {
         return Collections.emptyList();
--- task7 - Swagger/src/main/java/com/codecool/spingboot_tasks/swagger/model/Product.java
+++ task7 - Swagger/src/main/java/com/codecool/spingboot_tasks/swagger/model/Product.java
@@ -1,16 +1,10 @@
 package com.codecool.spingboot_tasks.swagger.model;
 
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
-@ApiModel(description = "Product base information")
 public class Product {
-    @ApiModelProperty(required = true, value = "Identifier")
     private long id;
-    @ApiModelProperty(required = true, value = "Name of the product")
     private String name;
-    @ApiModelProperty(value = "Price of the product")
     private int price;
 }
