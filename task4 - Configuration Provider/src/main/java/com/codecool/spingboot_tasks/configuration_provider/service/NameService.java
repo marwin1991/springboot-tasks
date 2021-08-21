3dfc4af641f85412225cbacb95c5d295c05da4db
Peter <peter.zmilczak@gmail.com>
2021-08-21 14:59:49 +0200

    SOme improvements

--- task4 - Configuration Provider/src/main/java/com/codecool/spingboot_tasks/configuration_provider/service/NameService.java
+++ task4 - Configuration Provider/src/main/java/com/codecool/spingboot_tasks/configuration_provider/service/NameService.java
@@ -1,15 +1,11 @@
 package com.codecool.spingboot_tasks.configuration_provider.service;
 
-import com.google.gson.Gson;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class NameService {
 
-    private static final String name = "CodeCool"
+    private static final String name = "CodeCool";
 
     public String getName(){
         return name;
